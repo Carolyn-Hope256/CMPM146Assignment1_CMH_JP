acd84b60c6bcaa8ca297e5faed832552c1557769
Carolyn-Hope256 <cmhope@ucsc.edu>
2025-05-02 09:38:09 -0700

    several severe oversights in our polygon finding algorithm. Will need more time to complete.

--- Assets/Scripts/assignment3/NavMesh.cs
+++ Assets/Scripts/assignment3/NavMesh.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Runtime.ConstrainedExecution;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Threading.Tasks;
 
 public class NavMesh : MonoBehaviour
 {
@@ -25,83 +26,146 @@ public class NavMesh : MonoBehaviour
         g.all_nodes = new List<GraphNode>();
         int idnum = 0;
 
+        //List of exterior and interior walls that is iterated through to find cuts.
+        //Interior walls are inserted after the wall that "called" them for sake of the algorithm.
+        List<Wall> Walls = new List<Wall>();
+        Walls.Clear();
+        
         //Master list of actual exterior and interior walls. Append-only.
         //A wall's index in this list functions as a sort of ID number.
-        List<Wall> walls = new List<Wall>();
-
+        List<Wall> Canon = new List<Wall>();
+        Canon.Clear();
+        
         //A list of int lists, each representing a polygon. Each integer entry coresponds to a wall entry in the master list.
         List<List<int>> Polygons = new List<List<int>>();
+        Polygons.Clear();
 
         //Create the first polygon, and stock it with edge wall IDs
-        //and the canon list with the actual perimeter walls.
+        //and the Canon list with the actual perimeter walls.
         Polygons.Add(new List<int>());
         for (int i = 0; i < outline.Count; i++)
         {
-            walls.Add(outline[i]);
+            Walls.Add(outline[i]);
+            Canon.Add(outline[i]);
             Polygons[0].Add(i);
         }
 
         //Traverse along the list of walls, searching for reflex angles and cutting appropriately
         //Much of this code is probably unnecessary, left over from when I was still troubleshooting cuts
-        for (int i = 0; i < walls.Count; i++)
+        for (int i = 0; i < Walls.Count; i++)
         {
-
+            //Debug.DrawRay(Walls[i].start, Walls[i].direction * Walls[i].length, Color.green, 1);
+            //await Task.Delay(1000);
             bool fanning = false;
-            if((walls[i].start - walls[GetWallIndex(i, 1, walls.Count)].start).magnitude < Mathf.Epsilon)
+            if((Walls[i].start - Walls[GetWallIndex(i, 1, Walls.Count)].start).magnitude < Mathf.Epsilon)
             {
                 fanning = true;
             }
-
+            
             //If we've arrived at a reflex angle...
-            if (IsReflex(walls[i], walls[GetWallIndex(i, 1, walls.Count)], fanning))
+            if (IsReflex(Walls[i], Walls[GetWallIndex(i, 1, Walls.Count)], fanning))
             {
                 Debug.Log("Vertex at the end of wall " + i + " is a reflex angle!");
                 //GameObject marker = (GameObject)Instantiate(markerprefab, walls[i].end, Quaternion.identity);
                 //marker.transform.localScale = new Vector3(10, 10, 10);
                 //marker.GetComponent<BreadcrumbController>().lifetime = 20;
 
+                //Initialize candidate information.
                 Vector3 candidate = new Vector3(0, 0, 0);
                 float bestAngle = 180f;
                 bool foundCandidate = false;
                 
-                for(int j = 2; j < walls.Count; j++)
+                //Skip ahead to the first vertex that could be drawn to and form a polygon, and begin searching for candidates.
+                for(int j = 2; j < Walls.Count; j++)
                 {
-                    Wall cur = walls[GetWallIndex(i, j, walls.Count)];
+                    Wall cur = Walls[GetWallIndex(i, j, Walls.Count)];
                     
-                    float a = Vector3.SignedAngle(walls[i].direction, (cur.end - walls[GetWallIndex(i, 1, walls.Count)].start).normalized, Vector3.up);
-                    if(a <= 0 && Mathf.Abs(a) < bestAngle && IsValid(new Wall(walls[GetWallIndex(i, 1, walls.Count)].start, cur.end), walls))//todo: check if proposed wall intersects/is a duplicate
+                    //A potential candidate vector must have a non-positive angle (so that further reflexes aren't created),
+                    //favoring angles closer to 0, and must not overlap or intersect existing walls
+                    float a = Vector3.SignedAngle(Walls[i].direction, (cur.end - Walls[GetWallIndex(i, 1, Walls.Count)].start).normalized, Vector3.up);
+                    if(a <= 0 && Mathf.Abs(a) < bestAngle && IsValid(new Wall(Walls[GetWallIndex(i, 1, Walls.Count)].start, cur.end), Walls))//todo: check if proposed wall intersects/is a duplicate
                     {
-                        Debug.DrawRay(walls[i].end, walls[i].direction * 10, Color.green, 1000);
-                        Debug.DrawRay(walls[i].end, (cur.end - walls[i].end), Color.blue, 1000);
-                        Debug.Log("Found new best candidate after" + j + "jumps of angle " + a);
+                        Debug.DrawRay(Walls[i].end, Walls[i].direction * 10, Color.green, 1000);
+                        Debug.DrawRay(Walls[i].end, (cur.end - Walls[i].end), Color.blue, 1000);
+                        //Debug.Log("Found new best candidate after" + j + "jumps of angle " + a);
                         candidate = cur.end;
                         bestAngle = a;
                         foundCandidate = true;
                     }
                 }
-
+                
+                
+                //If candidate found, then slice
                 if (foundCandidate)
                 {
                     //Instantiate(markerprefab, candidate, Quaternion.identity);
                     
-                    List<Wall> Poly = new List<Wall>();
                     int jumps = 1;
-                    for (int j = 1; j < walls.Count; j++)
+
+                    
+                    List<int> newPoly = new List<int>();
+                    int cutPoly = 0;
+
+                    Debug.Log("Creating Polygon " + Polygons.Count);
+
+                    for (int j = 1; j < Walls.Count; j++)
                     {
                         jumps++;
-                        Wall cur = walls[GetWallIndex(i, j, walls.Count)];
-                        Poly.Add(cur);
+
+                        Wall cur = Walls[GetWallIndex(i, j, Walls.Count)];
+                        //Debug.DrawRay(cur.start, cur.direction * cur.length, Color.green, 1);
+                        int cInd = GetCanonWallIndex(cur, Canon);
+                        if(cInd < 0)
+                        {
+                            //Debug.Log("Couldn't find wall " + GetWallIndex(i, j, Walls.Count) );
+                        }
+
+                        newPoly.Add(cInd);
+
+                        for (int p = 0; p < Polygons.Count; p++)
+                        {
+                            if (Polygons[p].Contains(cInd) && Mathf.Abs(cInd) < outline.Count)
+                            {
+                                Debug.Log("Cutting wall " + cInd + " from Polygon " + p);
+                                cutPoly = p;
+                                Polygons[p].Remove(cInd);
+                            }
+                        }
+
+
                         if ((cur.end - candidate).magnitude < Mathf.Epsilon)
                         {
                             break;
                         }
+                        
                     }
-                    walls.Insert(GetWallIndex(i, 1, walls.Count), new Wall(candidate, walls[GetWallIndex(i, 1, walls.Count)].start));
-                    Poly.Add(walls[GetWallIndex(i, 1, walls.Count)]);
-                    //Poly.Add(new Wall(candidate, walls[i].end));
+                    
+
+                    Canon.Add(new Wall(candidate, Walls[GetWallIndex(i, 1, Walls.Count)].start));
+                    Walls.Insert(GetWallIndex(i, 1, Walls.Count), new Wall(candidate, Walls[GetWallIndex(i, 1, Walls.Count)].start));
+                    
+                    Debug.Log("Walls length: " + Walls.Count + ". Canon Length: " + Canon.Count);
+                    //Polygons[cutPoly].Add(-1 * (Canon.Count - 1));
+                    newPoly.Add(Canon.Count - 1);
+
+                    string polycontents = "Created new poly " + Polygons.Count + ": ";
+                    foreach (int w in newPoly)
+                    {
+                        polycontents += w + ", ";
+                    }
+                    Debug.Log(polycontents);
+                    polycontents = "Old poly " + cutPoly + " now contains: ";
+                    foreach (int w in Polygons[cutPoly])
+                    {
+                        polycontents += w + ", ";
+                    }
+                    Debug.Log(polycontents);
+
+                    Polygons.Add(newPoly);
                     //g.all_nodes.Add(new GraphNode(idnum, Poly));
                     //idnum++;
                 }
+                //await Task.Delay(1000);
 
 
 
@@ -116,18 +180,46 @@ public class NavMesh : MonoBehaviour
             List<Wall> TruePoly = new List<Wall>();
             for(int i = 0; i < Polygons[p].Count; i++)
             {
-                printPoly += Polygons[p][i] + ", ";
-                TruePoly.Add(walls[Polygons[p][i]]);
+                int w = Polygons[p][i];
+                bool flip = false;
+
+                if(w < 0)
+                {
+                    w *= -1;
+                    flip = true;
+                }
+
+                //Debug.Log(w);
+                printPoly += w + ", ";
+                if (flip)
+                {
+                    TruePoly.Add(FlipWall(Canon[w]));
+                }
+                else
+                {
+                    TruePoly.Add(Canon[w]);
+                }
+                
             }
+            Debug.Log(printPoly);
+
+            /*for(int i = 0; i < TruePoly.Count; i++) 
+            {
+                
+            }*/
 
             //And add it to the graph.
-            //g.all_nodes.Add(new GraphNode(p, TruePoly));
+            if(TruePoly.Count > 2)
+            {
+                //g.all_nodes.Add(new GraphNode(p, TruePoly));
+            }
+            
         }
         
-        //Debug, draws canon walls via rays
-        for (int i = 0; i < walls.Count; i++)
+        //Debug, draws Canon walls via rays
+        for (int i = 0; i < Walls.Count; i++)
         {
-            Debug.DrawRay(walls[i].start, walls[i].direction * walls[i].length, Color.yellow, 3000);
+            Debug.DrawRay(Canon[i].start, Canon[i].direction * Canon[i].length, Color.yellow, 5);
         }
         return g;
     }
@@ -160,20 +252,73 @@ public class NavMesh : MonoBehaviour
         return target;
     }
 
+    public Wall NextWall(Wall wall, List<Wall> scope) {
+        List<Wall> candidates = new List<Wall>();
+        float bestAngle = 180;
+        Wall best = null;
+        foreach(Wall w in scope)
+        {
+            if((wall.end - w.start).magnitude < Mathf.Epsilon && Vector3.SignedAngle(w.direction, wall.direction, Vector3.up) < bestAngle)
+            {
+                best = w;
+                bestAngle = Vector3.SignedAngle(w.direction, wall.direction, Vector3.up);
+
+            }
+        }
+        return best;
+    }
+
+    //Given a wall, grabs it's index in the canon list
+    public int GetMapWallIndex(Wall wall, List<Wall> walls)
+    {
+
+        for (int i = 0; i < walls.Count; i++)
+        {
+            if (wall.Same(walls[i]))
+            {
+                return i;
+            }
+        }
+
+        Debug.LogError("Wall not in Walls!");
+        return -1;
+
+    }
+
+    //Given a wall, grabs it's index in the canon list
+    public int GetCanonWallIndex(Wall wall, List<Wall> canon) { 
+        
+        for (int i = 0; i < canon.Count; i++) {
+            if (wall.Same(canon[i])) 
+            { 
+                return i;
+            }
+        }
+
+        Debug.LogError("Wall not in canon!");
+        return -1;
+
+    }
+
+    public Wall FlipWall(Wall wall) 
+    { 
+        return new Wall(wall.end, wall.start);
+    }
+
     public bool IsValid(Wall wall, List<Wall> existing)
     {
-        Debug.Log("found potential new wall");
+        //Debug.Log("found potential new wall");
         for (int i = 0; i < existing.Count; i++)
         {
             if (wall.Crosses(existing[i]) && !SharesPoint(wall, existing[i]))
             {
 
-                Debug.Log("new wall crosses wall " + i + "!");
+                //Debug.Log("new wall crosses wall " + i + "!");
                 return false;
             }
             if (wall.Same(existing[i]))
             {
-                Debug.Log("new wall is the same as wall " + i + "!");
+                //Debug.Log("new wall is the same as wall " + i + "!");
                 return false;
             }
         }
@@ -217,6 +362,9 @@ public class NavMesh : MonoBehaviour
     public void SetMap(List<Wall> outline)
     {
         Graph navmesh = MakeNavMesh(outline);
+        /*MakeNavMesh(outline);
+        Graph navmesh = new Graph();
+        navmesh.all_nodes = new List<GraphNode>();*/
         if (navmesh != null)
         {
             Debug.Log("got navmesh: " + navmesh.all_nodes.Count);
